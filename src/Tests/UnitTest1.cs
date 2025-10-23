3cca7fc00d0b1b873da390104a8de7d06be8f5b1
Phillip Pletbjerg Sveistrup <ppsv70676@edu.ucl.dk>
2025-10-23 12:35:13 +0200

    test also

---
+++ src/Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Moq;
+
+namespace Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			var a = 5;
+			var b = 3;
+
+
+
+			var result = a + b;
+
+
+			result.Should().Be(8);
+
+
+		}
+	}
+}
